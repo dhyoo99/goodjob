b29580944450f76e5b81994c31ab27aca2dd11eb
dhyoo99 <dhyoo99@naver.com>
2020-11-15 11:30:27 +0900

    deleted reactapp file

--- backend/urls.py
+++ backend/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from app import views
 # from django.conf.urls import include
 
 urlpatterns = [
